adb67f43d83896220ce6662dc7fdb03e1e9ea9ba
Giaanh0509 <ngotthe0509.com>
2024-05-14 23:31:03 +0700

    add base code

---
+++ src/backend/Site.java
@@ -0,0 +1,12 @@
+package backend;
+
+import java.util.ArrayList;
+
+public class Site {
+    private String code;
+    private String deliveryInfo;
+    private String name;
+    private String otherInfo;
+    ArrayList<Merchandise> merchandisesList;
+
+}

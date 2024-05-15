ee6840e490e16b119101cbe2e9f2c21aafb9e70d
Giaanh0509 <ngotthe0509.com>
2024-05-15 10:22:34 +0700

    add RawMerchandiseDatabase

---
+++ src/backend/RawMerchandiseDabatase.java
@@ -0,0 +1,5 @@
+package backend;
+
+public interface RawMerchandiseDabatase {
+    RawMerchandise findRawMerchandise(String merchandiseCode);
+}
--- src/backend/SalesDepartment.java
+++ src/backend/SalesDepartment.java
@@ -2,13 +2,24 @@ package backend;
 
 import java.util.ArrayList;
 
-public class SalesDepartment {
+public class SalesDepartment implements RawMerchandiseDabatase {
     private ArrayList<Order> orderList;
-    public SalesDepartment() {
 
+    public SalesDepartment() {
+        this.orderList = new ArrayList<>();
     }
+
     public SalesDepartment(ArrayList<Order> orderList) {
         this.orderList = orderList;
+    }
+
+    public void createOrder(Order order) {
+
+    }
+
+    @Override
+    public RawMerchandise findRawMerchandise(String merchandiseCode) {
+        return;
     }
 }
 

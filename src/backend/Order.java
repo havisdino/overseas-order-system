471be67cfc779f335e4ee1be785a3096c4e20837
havisdino <hadinh893@gmail.com>
2024-05-20 01:46:34 +0700

    Add config singleton

--- src/backend/Order.java
+++ src/backend/Order.java
@@ -7,6 +7,7 @@ public class Order {
     private String id;
     private List<Merchandise> merchandiseList;
     private Date dateCreate;
+
     public Order(String id, List<Merchandise> merchandiseList, Date dateCreate) {
         this.id = id;
         this.merchandiseList = merchandiseList;
